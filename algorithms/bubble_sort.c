83cb2ec4520930070165a7627984610b2950192a
Sordid-Slug <126084827+Sordid-Slug@users.noreply.github.com>
2023-10-20 19:34:02 +0300

    Update bubble_sort.c

--- algorithms/bubble_sort.c
+++ algorithms/bubble_sort.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
-#define MAS_LEN 10
+#include <stdlib.h>
 
-int input(int *mas);
-void swap();
-void bubble_sort(int *mas);
-void output(int *mas);
+void input(int **mas, int *MAS_LEN, int *flag);
+void swap(int *el_l, int *el_r);
+void bubble_sort(int *mas, int n);
+void output(int *mas, int n);
 
 int main() {
-    int mas[MAS_LEN];
-    if (input(mas) > 0) {
+    int *mas, MAS_LEN, flag = 0;
+    input(&mas, &MAS_LEN, &flag);
+    if (flag > 0) {
+        printf("n/a");
         return 0;
     }
-    bubble_sort(mas);
-    output(mas);
+    bubble_sort(mas, MAS_LEN);
+    output(mas, MAS_LEN);
+    free(mas);
     return 0;
 }
 
-int input(int *mas) {
-    printf("Enter a numbers: ");
-    for (int i = 0; i < MAS_LEN; i++) {
-        if ((scanf("%d", mas + i) != 1 || (i == MAS_LEN - 1 && getchar() != '\n')) ||
-            (i != MAS_LEN - 1 && getchar() != ' ')) {
-            printf("n/a");
-            return 1;
-        }
+void input(int **mas, int *MAS_LEN, int *flag) {
+    printf("Enter: ");
+    if (scanf("%d", MAS_LEN) != 1 && getchar() != '\n') {
+        *flag = 1;
+        return;
     }
-    return 0;
+    *mas = (int *)malloc(*MAS_LEN * sizeof(int));
+    if (*mas) {
+        for (int i = 0; i < *MAS_LEN; i++) {
+            if (scanf("%d", *mas + i) != 1) {
+                *flag = 2;
+                break;
+            }
+        }
+    } else
+        *flag = 4;
 }
 void swap(int *el_l, int *el_r) {
     int temp = *el_l;
@@ -33,7 +42,7 @@ void swap(int *el_l, int *el_r) {
     *el_r = temp;
 }
 
-void bubble_sort(int *mas) {
+void bubble_sort(int *mas, int MAS_LEN) {
     for (int i = 0; i < MAS_LEN; i++) {
         for (int j = 0; j < MAS_LEN - i - 1; j++) {
             if (mas[j] > mas[j + 1]) {
@@ -43,7 +52,7 @@ void bubble_sort(int *mas) {
     }
 }
 
-void output(int *mas) {
+void output(int *mas, int MAS_LEN) {
     for (int i = 0; i < MAS_LEN; i++) {
         printf("%d ", mas[i]);
     }
