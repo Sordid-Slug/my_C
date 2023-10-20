bf584c5bf3cb22458d5ea60b042425e3137be900
Sordid-Slug <126084827+Sordid-Slug@users.noreply.github.com>
2023-10-20 20:37:20 +0300

    Create MyStruct.h

---
+++ structures/MyStruct.h
@@ -0,0 +1,15 @@
+#ifndef MYSTRUCT_H
+#define MYSTRUCT_H
+
+struct Data {
+    int year;
+    int month;
+    int day;
+    int hour;
+    int min;
+    int sec;
+    int status;
+    int code;
+};
+
+#endif
